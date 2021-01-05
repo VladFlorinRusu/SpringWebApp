99e5d649e43c8801c17e43fe044765133e6f2a16
vladrusu <vladrusu7.vr@gmail.com>
2021-01-05 11:34:31 +0200

    first attemp

---
+++ src/main/java/com/vlad/HelloController.java
@@ -0,0 +1,20 @@
+package com.vlad;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+
+    @RequestMapping("/home")
+    public String home() {
+        return "Another text here";
+    }
+
+}
