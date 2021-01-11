b363727ebccfc7cff1cb59ca1ee1086f214061a6
vladrusu <vladrusu7.vr@gmail.com>
2021-01-11 21:53:07 +0200

    "I have added Local time to see the changes"

--- src/main/java/com/vlad/HelloController.java
+++ src/main/java/com/vlad/HelloController.java
@@ -3,12 +3,14 @@ package com.vlad;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.time.LocalDateTime;
+
 @RestController
 public class HelloController {
 
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "Hello! The time is "+ LocalDateTime.now();
     }
 
 
