aab2402503ad66b12d54fc9548ec96c50e439a86
vladrusu <vladrusu7.vr@gmail.com>
2021-01-11 23:03:56 +0200

    "WebPage using view"

--- src/main/java/com/vlad/HelloController.java
+++ src/main/java/com/vlad/HelloController.java
@@ -1,12 +1,27 @@
 package com.vlad;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.time.LocalDateTime;
 
-@RestController
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Controller
 public class HelloController {
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        model.addAttribute("myList" , List.of("first name", "Age","City"));
+        return "greeting";
+    }
+
 
     @RequestMapping("/")
     public String index() {
